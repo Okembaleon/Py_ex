2433691e1aa705ad6be009571af20f397f8d7d06
leon okemba <>
2022-10-26 19:59:17 +0300

    Py_ex

---
+++ 1_output.py
@@ -0,0 +1,7 @@
+#Output in python
+#Print is used to display output on the screen
+print("Hello World")
+
+print(2+2)
+
+print (9%4)
---
+++ 2_datatypes.py
@@ -0,0 +1,25 @@
+x = 5
+y = 2.5
+
+# Python allows variable declarations on the same line
+x, y = 5, 2.5
+
+# Python allows setting multiple variables to the same value
+
+a = b = c = 3
+
+my_string = ("Hello There")
+is_true = True
+is_false = False
+
+my_string = [1, 2, 3]
+none_type = None
+
+my_char = ('a')
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(my_string))
+print(type(is_true))
+print(type(my_char))
